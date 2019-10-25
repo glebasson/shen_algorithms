c8341ba44dbf61032e2fed2b762ee8bb918ee1ee
glebasson <glebasson@yandex.ru>
2019-10-25 04:26:47 +0300

    solved continius backpack problem

---
+++ greedy/backpack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <utility>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <iterator> 
+#include <iomanip>
+
+using P = std::pair<int,double>;
+
+double maximize_backpack(std::vector<P> v, int &maxweight) {
+  int weight = 0;
+  double value = 0;
+  for (auto &el : v) {
+    if ( (weight + el.second) <= maxweight) {
+      value += el.first;
+      weight += el.second;
+    } else {
+      value += el.first/el.second * (maxweight - weight);
+      break;
+    }
+  }
+  return value;
+}
+
+int main(void) {
+  int n, w;
+  std::cin >> n;
+  std::cin >> w;
+
+  std::vector<std::pair<int,double>> cw(n);
+  int tmp;
+  for ( auto &el : cw ) {
+    std::cin >> el.first;
+    std::cin >> el.second;
+  }
+
+  std::sort(cw.begin(), cw.end(), [](P &left, P &right){
+    return (left.first)/left.second > (right.first)/right.second;
+  });
+
+  double max = maximize_backpack(std::move(cw), w);
+  std::cout << std::fixed << std::setprecision(3) << max << std::endl; 
+
+}
