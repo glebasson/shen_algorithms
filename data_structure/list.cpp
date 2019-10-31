373592d4f39c27884e742c556c52d1b8da9c41f9
glebasson <glebasson@yandex.ru>
2019-10-31 18:16:49 +0300

    bracket problem solved using stack implementation

---
+++ data_structure/list.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+template <typename T>
+struct node {
+	T value;
+	int num;
+	node *prev;
+};
+
+template <typename T>
+struct stack {
+	node<T> *head = NULL;
+	int length = 0;
+	void push(const int &value) {
+		node<T> *el = new node<T>;
+		(*el).value = value;
+		(*el).prev = head;
+		head = el;
+		length++;
+	}
+
+	int pop() {
+		int a = head->value;
+		head = head->prev;
+		length--;
+		return(a);
+	}
+
+	int top() {
+		return(head->value);
+	}
+
+	int size() {
+		return(length);
+	}
+};
+
+int main() {
+	stack<int> st;
+	int count = 0;
+	char c;
+	while (std::cin >> c) {
+		count++;
+		if (c == '[' || c == '{' || c == '(') {
+			st.push(c);
+			st.head->num = count;
+		}
+		else {
+			if (st.head == NULL){
+				std::cout << count << std::endl;
+				return(0);
+			}
+			int top = st.top();
+			if ((top == '[' && c == ']') || \
+				(top == '{' && c == '}') || \
+				(top == '(' && c == ')')) {
+				st.pop();
+			} else {
+				std::cout << count << std::endl;
+				return(0);
+			}
+		}
+	}
+	if (st.size()) {
+		std::cout << st.head->num << std::endl;
+		return(0);
+	}
+	std::cout << "Success" << std::endl;
+
+	return(0);
+}
+
