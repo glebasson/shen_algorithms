90041884c478bd0e9438763c690746cadf8a4a29
glebasson <glebasson@yandex.ru>
2020-06-29 20:14:21 +0300

    Shared lib demo

---
+++ linux/libdemo/hello.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+#include <stdio.h>
+
+void hello_message(char const* name) {
+	printf("Hello %s!\n", name);
+}
---
+++ linux/libdemo/main.c
@@ -0,0 +1,6 @@
+#include "hello.h"
+
+int main() {
+	hello_message("Dima");
+	return 0;
+}
