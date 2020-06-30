eba26c305a4eb5e1615d8545996f6c9a12e20397
glebasson <glebasson@yandex.ru>
2020-06-30 22:17:45 +0300

    runtime linking demo

--- linux/libdemo/main.c
+++ linux/libdemo/main.c
@@ -1,6 +1,27 @@
-#include "hello.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <dlfcn.h>
+
+void (*hello_message)(char const*);
+
+bool init_library() {
+	void * hdl = dlopen("./libHello.so", RTLD_LAZY);
+	if(hdl == NULL) {
+		return false;
+	}
+	hello_message = (void (*)(char const*))dlsym(hdl, "hello_message");
+	if(hello_message == NULL) {
+		return false;
+	}
+	return true;
+}
 
 int main() {
-	hello_message("Dima");
+	if (init_library()) {
+		hello_message("Dima");
+	} else {
+		printf("Library was not loaded!\n");
+	}
 	return 0;
 }
