e07e0eaf43921da4f62e43850e0543b036c2c623
glebasson <glebasson@yandex.ru>
2020-07-07 02:10:20 +0300

    simple pstree implementation

--- linux/findppid/solution.c
+++ linux/findppid/solution.c
@@ -6,25 +6,36 @@
 
 #define BUF_SIZE 255
 
-int main() {
-    char path[BUF_SIZE];
-    sprintf(path, "/proc/%d/stat", getpid());
-    FILE * f = fopen(path, "r");
+pid_t findPPid(pid_t pid) {
+    char str[BUF_SIZE];
+    sprintf(str, "/proc/%d/stat", pid);
+    FILE * f = fopen(str, "r");
     if (f == NULL) {
         perror("open");
         return EXIT_FAILURE;
     }
-    char str[BUF_SIZE];
-    if (fgets(str, BUF_SIZE, f) == NULL) {
-        perror("gets");
+    if (fscanf(f, "%*s %*s %*s %i", &pid) == EOF) {
+        perror("scanf");
         return EXIT_FAILURE;
     }
-    char * p = strtok(str, " ");
-    int i;
-    for (i = 0; i < 2; i++) {
-        p = strtok(NULL, " "); 
+    if (fclose(f) == EOF) {
+        perror("close");
+        return EXIT_FAILURE;
     }
-    p = strtok(NULL, " ");
-    printf("%s\n", p);
-    fclose(f);
+    return pid;
+}
+
+void printPidTree(pid_t pid) {
+    printf("%d\n", pid);
+    pid_t ppid = findPPid(pid);
+    if (ppid) {
+        printPidTree(ppid);
+    }
+}
+
+int main(int argc, char const* argv[]) {
+    pid_t pid;
+    sscanf(argv[1], "%i", &pid);
+    printPidTree(pid);
+    return EXIT_SUCCESS;
 }
