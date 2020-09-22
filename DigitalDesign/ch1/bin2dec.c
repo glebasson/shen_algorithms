3ffeb0ddbbc731f3eefd9db1475504804a4dfbb6
glebasson <glebasson@yandex.ru>
2020-09-22 14:19:39 +0300

    EX:1.69, p:170 // bin2dec

---
+++ DigitalDesign/ch1/bin2dec.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const argv[]) {
+    char input[255];
+    scanf("%s", &input);
+    unsigned int out = 0U;
+    for(int i=0; input[i]; i++) {
+        out = out * 2 + (input[i] - 0x30);
+    }
+    printf("%u\n", out);
+    return(0);
+}
