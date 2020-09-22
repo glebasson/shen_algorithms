42fbca6016600bc1eeb5305ba6278233bb4d2bc8
glebasson <glebasson@yandex.ru>
2020-09-22 14:31:56 +0300

    postincrement to preincrement

--- DigitalDesign/ch1/bin2dec.c
+++ DigitalDesign/ch1/bin2dec.c
@@ -4,7 +4,7 @@ int main(int argc, char const argv[]) {
     char input[255];
     scanf("%s", &input);
     unsigned int out = 0U;
-    for(int i=0; input[i]; i++) {
+    for(int i=0; input[i]; ++i) {
         out = out * 2 + (input[i] - 0x30);
     }
     printf("%u\n", out);
